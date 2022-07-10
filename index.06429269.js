424565bbe082eccc86d7260b9b19394b640c7327
Qweeqer <103505333+Qweeqer@users.noreply.github.com>
2022-07-10 16:21:15 +0000

    Deploying to gh-pages from @ Qweeqer/hellish-english@e80c541b4e093fd6f5812f7f1639f41aace4fffd 🚀

---
+++ index.06429269.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuModalBtn:document.querySelector("[data-menu-modal-open]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};e.openMenuModalBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=index.06429269.js.map
---
+++ index.2a00662b.js
@@ -0,0 +1,2 @@
+function readMore(){var e=document.getElementById("dots"),n=document.getElementById("more"),t=document.getElementById("btn");"none"===e.style.display?(e.style.display="inline",t.innerHTML="Read more",n.style.display="none"):(e.style.display="none",t.innerHTML="Close",n.style.display="inline")}
+//# sourceMappingURL=index.2a00662b.js.map
